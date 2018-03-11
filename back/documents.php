b6914ec48482d9c52d3c58f2ea8039f99366a6d1
Riccardo Bachis <dydrich@yahoo.com>
2018-03-11 17:42:22 +0100

    modifiche grafiche menu ordinamento documenti

--- back/documents.php
+++ back/documents.php
@@ -19,13 +19,32 @@ $me = $_SESSION['__user__']->getUid();
 /*
  * order
  */
-$o = 'document_name';
+$o = 'title';
 $d = 'asc';
+$label = 'Nome';
 if (isset($_GET['o'])) {
 	$o = $_GET['o'];
 }
 if (isset($_GET['d'])) {
 	$d = $_GET['d'];
+}
+
+switch ($o) {
+	case 'title':
+		$label = 'Nome';
+		break;
+	case 'category':
+		$label = 'Categoria';
+		break;
+	case "subject":
+		$label = 'Disciplina';
+		break;
+	case 'upload_date':
+		$label = 'Data caricamento';
+		break;
+	case "last_modified_time":
+		$label = 'Ultima modifica';
+		break;
 }
 
 $drawer_label = "Documenti";
@@ -43,18 +62,28 @@ else {
 	/*
 	 * order link
 	 */
-	$name_link = "documents.php?d=desc";
+	$name_link = "documents.php?o=".$o."&d=desc";
 	$arrow = 'arrow_downward';
 	if ($d == 'desc') {
-		$name_link = "documents.php?d=asc";
+		$name_link = "documents.php?o=".$o."&d=asc";
 		$arrow = 'arrow_upward';
 	}
 }
-
+$sql = "SELECT rb_documents.*, rb_categories.color AS color, rb_categories.icon AS icon, rb_subjects.name as sub 
+		FROM rb_documents, rb_subjects, rb_categories 
+		WHERE owner = $me 
+		AND subject = sid 
+		AND category = cid
+		ORDER BY $o $d";
+$sel_categorie = "SELECT * FROM rb_categories ORDER BY name";
 try {
-	$res_docs = $db->executeQuery("SELECT rb_documents.*, rb_subjects.name as sub FROM rb_documents, rb_subjects WHERE owner = $me AND subject = sid ORDER BY $o $d");
+	$res_docs = $db->executeQuery($sql);
+	$res_categorie = $db->executeQuery($sel_categorie);
+
 } catch (\edocs\MySQLException $ex) {
 
 }
+
+$cat_menu_h = $res_categorie->num_rows * 50;
 
 include "documents.html.php";
