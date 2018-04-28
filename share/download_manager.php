4b85996c8b58ab37d84c15c2fb566fa94d6ace5d
Riccardo Bachis <dydrich@yahoo.com>
2018-04-28 19:46:59 +0200

    gestione registrazione download

--- share/download_manager.php
+++ share/download_manager.php
@@ -6,9 +6,13 @@ require_once "../lib/Document.php";
 ini_set('display_errors', 1);
 
 $document = null;
+$register = false;
+if (isset($_REQUEST['register']) && $_REQUEST['register'] == 1) {
+	$register = true;
+}
 $document = new \edocs\Document($_GET['did'], null, null, new MYSQLDataLoader($db));
 try{
-	$document->download();
+	$document->download($register);
 } catch (\edocs\MySQLException $ex){
 	echo "kosql|".$ex->getQuery()."|".$ex->getMessage();
 	exit;
